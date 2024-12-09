6ee48cfcc229e8ce3f6bd4432de3601afbdb2782
Berkin Demirel <berkin.koca01@gmail.com>
2024-12-09 22:38:31 +0300

    Added sounds, images, and sample JSON files to the project. General file structure adjustments made.

---
+++ HarryPotterQuiz/Support/HarryPotterQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterQuizApp.swift
+//  HarryPotterQuiz
+//
+//  Created by Berkin Demirel on 9.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

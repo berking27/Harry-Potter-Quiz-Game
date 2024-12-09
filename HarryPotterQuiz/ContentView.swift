71b1339beeb3923dcaa71fcb2f82f8fb5e030b1c
Berkin Demirel <berkin.koca01@gmail.com>
2024-12-09 22:21:35 +0300

    initial commit

---
+++ HarryPotterQuiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HarryPotterQuiz
+//
+//  Created by Berkin Demirel on 9.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
